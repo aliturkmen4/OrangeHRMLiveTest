ad50e62bedf09b5d77cf8b2d1b4c2d39c268cff4
alitu <alym11731996@gmail.com>
2024-03-13 01:30:02 +0300

    Correct login

--- src/test/java/StepDefinitions/LoginPageStepDefinitons.java
+++ src/test/java/StepDefinitions/LoginPageStepDefinitons.java
@@ -5,6 +5,7 @@ import Utils.DriverFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPageStepDefinitons {
@@ -26,6 +27,35 @@ public class LoginPageStepDefinitons {
         loginPage.checkMessageAboutUsernameAndPassword(errormessage,errormessage2);
     }
 
+    @When("Write false {string} for username field")
+    public void writeFalseForUsernameField(String username) {
+        loginPage.writeFalseUsername(username);
+    }
+
+    @When("Write false {string} for password field")
+    public void writeFalseForPasswordField(String password) {
+        loginPage.writeFalsePassword(password);
+    }
+
+    @Then("Check {string} message about false username and false password")
+    public void checkMessageAboutFalseUsernameAndFalsePassword(String error) {
+        loginPage.checkMessageAboutFalseUsernameAndPassword(error);
+    }
+
+    @When("Write correct {string} for username field")
+    public void writeCorrectForUsernameField(String username) {
+        loginPage.writeCorrectUsername(username);
+    }
+
+    @When("Write correct {string} for password field")
+    public void writeCorrectForPasswordField(String password) {
+        loginPage.writeCorrectPassword(password);
+    }
+
+    @Then("Check the url to understand success login")
+    public void checkTheUrlToUnderstandSuccessLogin() {
+        loginPage.checkTheUrlToUnderstandSuccessLogin();
+    }
 }
 
 
